9639f3e8285a039917ab6f8142e8cc0ed5654d83
mjy191 <mjy191@163.com>
2022-09-13 15:33:25 +0800

    更新日志记录请求、返回数据日志

--- src/MyLogs.php
+++ src/MyLogs.php
@@ -1,11 +1,9 @@
 <?php
+
 namespace Mjy191\MyLogs;
 
-use App\Common\Tools;
-use Illuminate\Support\Facades\DB;
-use phpDocumentor\Reflection\DocBlock\Tags\Link;
-
-class MyLogs {
+class MyLogs
+{
 
     /**
      * 记录日志
@@ -13,41 +11,43 @@ class MyLogs {
      * @param $content
      * @return false|int
      */
-    public static function write($logName,$content){
+    public static function write($logName, $content)
+    {
         $logId = self::logId();
-        if(!is_string($content)){
+        if (!is_string($content)) {
             $content = self::toString($content);
         }
         $backtrace = debug_backtrace();
-        if(PHP_SAPI=='cli'){
-            $uri = 'cli['.implode(' ',$_SERVER['argv']).']';
-            $log_path=base_path().'/logs/cli/'.date('Ym');
-        }else{
-            $uri = 'uri['.$_SERVER['REQUEST_URI'].']';
-            $log_path=base_path().'/logs/'.date('Ym');
+        if (PHP_SAPI == 'cli') {
+            $uri = 'cli[' . implode(' ', $_SERVER['argv']) . ']';
+            $log_path = base_path() . '/logs/cli/' . date('Ym');
+        } else {
+            $uri = 'uri[' . $_SERVER['REQUEST_URI'] . ']';
+            $log_path = base_path() . '/logs/' . date('Ym');
         }
         $log_format = date('Y-m-d H:i:s')
-            ." {$uri}"
-            ." [{$backtrace[0]['file']}:{$backtrace[0]['line']}]"
-            ." cgi[".PHP_SAPI."]"
-            ." logId[{$logId}] "
-            ." {$logName}[{$content}]".PHP_EOL;
+            . " {$uri}"
+            . " [{$backtrace[1]['file']}:{$backtrace[1]['line']}]"
+            . " cgi[" . PHP_SAPI . "]"
+            . " logId[{$logId}] "
+            . " {$logName}[{$content}]" . PHP_EOL;
         $date = date('YmdH');
-        if(!is_dir($log_path)){
-            mkdir($log_path,0777,true);
+        if (!is_dir($log_path)) {
+            mkdir($log_path, 0777, true);
         }
-        return file_put_contents($log_path.'/'.$date.'.log', $log_format, FILE_APPEND);
+        return file_put_contents($log_path . '/' . $date . '.log', $log_format, FILE_APPEND);
     }
 
     /**
      * @param $param
      * @return false|string
      */
-    public static function toString($param){
-        if(is_string($param)){
+    public static function toString($param)
+    {
+        if (is_string($param)) {
             return $param;
         }
-        return json_encode($param,JSON_UNESCAPED_UNICODE);
+        return json_encode($param, JSON_UNESCAPED_UNICODE);
     }
 
     /**
@@ -56,23 +56,53 @@ class MyLogs {
      */
     public static function logId()
     {
-        if(!defined('logId')){
-            $logId = date('YmdHis').uniqid();
-            define('logId',$logId);
+        if (!defined('logId')) {
+            $logId = date('YmdHis') . uniqid();
+            define('logId', $logId);
         }
         return logId;
     }
 
     /**
-     * 记录db操作日志
      */
-    public static function recordDBlLog(){
-        DB::listen(function ($query) {
+
+    /**
+     * 记录db操作日志
+     * @param $db Illuminate\Support\Facades\DB;
+     */
+    public static function dBLog($db)
+    {
+        $db::listen(function ($query) {
             $tmp = str_replace('?', '"' . '%s' . '"', $query->sql);
             $sql = vsprintf($tmp, $query->bindings);
             $sql = str_replace("\\", "", $sql);
-            self::write('mysql',$sql);
+            self::write('mysql', $sql);
         });
     }
 
+    /**
+     * 使用中间件记录请求Logs.php
+     * @param $request
+     */
+    public static function reqLog($request)
+    {
+        $body = file_get_contents('php://input');
+        if($body){
+            self::write('request',$body);
+        }else{
+            self::write('request',$request->post());
+        }
+    }
+
+    /**
+     * 使用中间件记录返回数据Logs.php
+     * @param $request
+     * @param $response
+     */
+    public static function resLog($request, $response)
+    {
+        if($request->method()=='POST'){
+            self::write('response', $response->getContent());
+        }
+    }
 }
